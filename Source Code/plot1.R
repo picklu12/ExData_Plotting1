423991140a792d5cbceee5499e8cd79a3dfd8fa2
Raju Roy <raju.besu@gmail.com>
2018-09-08 22:47:03 -0700

    Assignment

--- Source Code/plot1.R
+++ Source Code/plot1.R
@@ -1,9 +1,10 @@
+#Store the dataset
 dataFile <- "household_power_consumption.txt"
-
+#read the file
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-#str(subSetData)
+#sote(subSetData)
 globalActivePower <- as.numeric(subSetData$Global_active_power)
 png("plot1.png", width=480, height=480)
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
